fbca3ef2c8613045909889104d9b27b0b7089ba5
Aashrith Kothuri <aashrith.kothuri@gmail.com>
2025-08-28 20:34:14 +0100

    Displaying actual speed (from raw bytes) in cm/s

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from time import sleep
 from machine import UART, Pin
 
+# Communication via UART
 uart = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))
 
 while True: 
     if uart.any():
-        line = uart.readline()
-        if not line:
+
+        # Getting UART data
+        data = uart.read()
+
+        # If line is falsy 
+        if not data:
             continue
-        print(line)
+
+        # If its valid data, print speed (increments of 52cm/s)
+        if len(data) == 10:
+            v = data[5]*256 + data[6] # converting corresponding bytes (big endian) to velocity 
+            print(f"speed: {v}")
+
+
     sleep(0.02)
