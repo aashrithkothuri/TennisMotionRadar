558f4f44c122970b65d2f76fd91bd9abeafab6d2
Aashrith Kothuri <aashrith.kothuri@gmail.com>
2025-09-01 14:04:28 +0100

    Implemented connect_known() method for wifi manager class

--- wifi.py
+++ wifi.py
@@ -1,12 +1,12 @@
 # Wifi manager class
 # Add a known_networks.json file in the following format to use connect_known() method:
 # [
-#   {"ssid":"Home","password":"homepass"},
-#   {"ssid":"Lab","password":"labpass"},
-#   {"ssid":"Hotspot","password":"12345678"}
+#   {"SSID":"Home","PASS":"homepass","IPCONFIG":(ip,subnetmask,gateway,DNS server)},
+#   {"SSID":"Lab","PASS":"labpass"},
+#   {"SSID":"Hotspot","PASS":"12345678"}
 # ]
 
-import network, uos as os, time
+import network, uos as os, time, ujson as json
 
 class wifiManager:
 
@@ -21,7 +21,7 @@ class wifiManager:
 
 
     # Method to connect to a specific wifi
-    def connect(self, SSID, PW):
+    def connect(self, SSID, PW, IPCONFIG):
 
 
 
@@ -55,21 +55,40 @@ class wifiManager:
 
                 if self.__wlan.status() < 0:
                     print("unable to connect: bad ssid/pass or other connect error")
-                    break
+                    return False
 
                 self.__wlan.disconnect()      
 
         # Displaying connected message
-        self.__wlan.ifconfig(('192.168.68.249', '255.255.255.0', '192.168.68.1', '8.8.8.8')) # hardcoded ipconfig
+        self.__wlan.ifconfig(IPCONFIG) # hardcoded ipconfig
         print(f"Connected to: {SSID}, config: {self.__wlan.ifconfig()}")
+        return True
 
     # Method to connect to known networks (defined by known_networks.json)
     def connect_known(self):
         
         try:
+
+            # Raises an error if known_networks.json doesn't exist
             os.stat("known_networks.json")
-        except:
-            return "unable to connect: no 'known_networks.json'"
+
+            # Opening json file
+            with open("known_networks.json", "r") as f:
+
+                # Parsing json to list (list of dict)
+                networks = json.load(f)
+                print(networks)
+
+                # trying to connect to each known network (in the order they are listed in)
+                for network in networks:
+                    SSID = network["SSID"]
+                    PASS = network["PASS"]
+                    IPCONFIG = eval(network["IPCONFIG"]) # Turns it into python tuple
+                    self.connect(SSID=SSID, PW=PASS, IPCONFIG=IPCONFIG)
+
+        except Exception as e:
+            print(e)
+            return False
 
     # Method to send data to an api via POST
     def send_POST(self, send_json, url):
@@ -82,4 +101,4 @@ class wifiManager:
         
 if __name__ == "__main__":
     wifi = wifiManager()
-    # wifi.connect("","") # -- works with actual wifi
+    wifi.connect_known()
